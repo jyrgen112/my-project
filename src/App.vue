a790cdfdb653b773a571cc103354feae91ae2f6f
jyrgen <jyrgen.vander@gmail.com>
2024-10-11 11:35:56 +0300

    panin koodi hakkama

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import comp from './comp.vue'
+</script>
+
+<template>
+  <div id="app">
+    <comp />
+  </div>
+</template>
---
+++ src/comp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box-container">
+    <div class=""></div>
+    <div class="big-box1"></div>
+    <div class="small-box1"></div>
+    <div class="small-box2"></div>
+    <div class="small-box3"></div>
+    <div class="big-box2"></div>
+  </div>
+</template>
+
+<style scoped>
+.box-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.big-box1 {
+  width: 40%;
+  height: 200px;
+  background-color: brown;
+  margin: 10px;
+  padding: 20px;
+  border: 1px solid #ddd;
+}
+
+.big-box2 {
+  width: 40%;
+  height: 200px;
+  background-color: #f0f0f0;
+  margin: 10px;
+  padding: 20px;
+  border: 1px solid #ddd;
+}
+
+.small-box {
+  width: 20%;
+  height: 100px;
+  background-color: #f0f0f0;
+  margin: 10px;
+  padding: 20px;
+  border: 1px solid #ddd;
+}
+</style>
